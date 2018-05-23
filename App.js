1b87e0cd3f758ebba15b4b5e0f0038a5c9ac0958
scottnicholasdalessandro <scottnicholasdalessandro@gmail.com>
2018-05-22 23:32:59 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {AsyncStorage} from 'react-native';
+
+export default class App extends React.Component {
+  componentWillMount() {
+    const DECKS_KEY = 'DecksKey';
+
+    function setDummyData() {
+      const dummyData = {
+        React: {
+          title: 'React',
+          questions: [
+            {
+              question: 'What is React?',
+              answer: 'A library for managing user interfaces'
+            },
+            {
+              question: 'Where do you make Ajax requests in React?',
+              answer: 'The componentDidMount lifecycle event'
+            }
+          ]
+        },
+        JavaScript: {
+          title: 'JavaScript',
+          questions: [
+            {
+              question: 'What is a closure?',
+              answer:
+                'The combination of a function and the lexical environment within which that function was declared.'
+            }
+          ]
+        }
+      };
+
+      return AsyncStorage.setItem(DECKS_KEY, JSON.stringify(dummyData));
+
+      
+    }
+
+    // why is the promise not fulfilled or rejected?
+    setDummyData()
+      .then(() => {
+        return AsyncStorage.getItem(DECKS_KEY);
+      })
+      .then(decks => {
+        console.log(decks);
+      })
+      .catch(err => console.log(err));
+      
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
